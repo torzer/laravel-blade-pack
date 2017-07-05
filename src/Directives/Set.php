bed4aa0a1384a69cf1142be3749d236e12824516
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2017-07-04 21:58:21 -0300

    Improve set then it can receiva a function as value

--- src/Directives/Set.php
+++ src/Directives/Set.php
@@ -18,7 +18,19 @@ class Set
     }
 
     public function getSource($expression) {
-        list($variable, $value) = explode(', ', str_replace(['(', ')'], '', $expression));
+        list($variable, $value) = explode(',', $expression, 2);
+
+        // Ensure variable has no spaces or apostrophes
+        $variable = trim(str_replace('\'', '', $variable));
+
+        // Make sure that the variable starts with $
+        if (! starts_with($variable, '$')) {
+            $variable = '$' . $variable;
+        }
+
+        $value = trim($value);
+
         return "<?php {$variable} = {$value}; ?>";
+
     }
 }
